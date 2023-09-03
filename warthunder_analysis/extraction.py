5ff8c5b32f455535cd1dc2af620a8a517ef97b5f
Andrew Jarvis <ajarvis7771@gmail.com>
2023-09-03 16:48:26 -0400

    add more data points

--- warthunder_analysis/extraction.py
+++ warthunder_analysis/extraction.py
@@ -11,22 +11,24 @@ BATTLE_RATING_TR = f'{GENERAL_INFO_2_DIV} > div.general_info_br > table > tbody 
 class GroundVehicleExtraction:
   def __init__(self, html: str) -> None:
     self.soup = BeautifulSoup(html, 'html5lib')
-    self.extractors = [
-      ('name', f'{MAIN_INFO_DIV} > div.general_info_name', self.__select_first_text),
-      ('country', f'{GENERAL_INFO_DIV} > div.general_info_nation > a:nth-child(2)', self.__select_first_text),
-      ('rank', f'{GENERAL_INFO_DIV} > div.general_info_rank > a:first-child', self.__select_first_text),
-      ('classification', f'{GENERAL_INFO_2_DIV} > div.general_info_class > div:last-child > a', self.__select_first_text),
-      ('battle_rating_AB', f'{BATTLE_RATING_TR} > td:nth-child(1)', self.__select_first_text),
-      ('battle_rating_RB', f'{BATTLE_RATING_TR} > td:nth-child(2)', self.__select_first_text),
-      ('battle_rating_SB', f'{BATTLE_RATING_TR} > td:nth-child(3)', self.__select_first_text),
-      ('purchase', f'{MAIN_INFO_DIV} > div.general_info_price > div > span.value', self.__select_first_text),
-    ]
-
-  def __select(self, *args):
-    return self.soup.css.select(*args)
+    self.extractors = {
+      'name': self.__select_first_text(f'{MAIN_INFO_DIV} > div.general_info_name'),
+      'country': self.__select_first_text(f'{GENERAL_INFO_DIV} > div.general_info_nation > a:nth-child(2)'),
+      'rank': self.__select_first_text(f'{GENERAL_INFO_DIV} > div.general_info_rank > a:first-child'),
+      'classification': self.__select_first_text(f'{GENERAL_INFO_2_DIV} > div.general_info_class > div:last-child > a'),
+      'battle_rating_AB': self.__select_first_text(f'{BATTLE_RATING_TR} > td:nth-child(1)'),
+      'battle_rating_RB': self.__select_first_text(f'{BATTLE_RATING_TR} > td:nth-child(2)'),
+      'battle_rating_SB': self.__select_first_text(f'{BATTLE_RATING_TR} > td:nth-child(3)'),
+      'purchase': self.__select_first_text(f'{MAIN_INFO_DIV} > div.general_info_price > div > span.value'),
+      'armour_hull': self.__select_armour('Hull'),
+      'armour_turret': self.__select_armour('Turret'),
+    }
   
-  def __select_first_text(self, *args):
-    return self.__select(*args)[0].get_text()
+  def __select_armour(self, name):
+    return lambda: self.soup.find('span', {'class': 'name'}, text=name).find_next_sibling('span').get_text()
+  
+  def __select_first_text(self, selector):
+    return lambda: self.soup.css.select_one(selector).get_text()
 
   def extract_all(self):
-    return { data_point: func(selector) for data_point, selector, func in self.extractors }
+    return { data_point: func() for data_point, func in self.extractors.items() }
